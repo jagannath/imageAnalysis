d87f812f060820bf7a3482fa690ff80ad6143d97
Jagannath Swaminathan <jagannath@marcottelab.org>
2014-09-01 17:45:10 -0500

    A large part of the photobleaching script has been coded. The README file is updated with the two scripts. The scripts now would be able to analyse trace files and study the peaks in every frame

--- pbKinetics.py
+++ pbKinetics.py
@@ -10,7 +10,6 @@ of that frame.
 
 import scipy.misc
 import numpy as np
-import scipy as sp
 import os
 import sys
 from subprocess import call
@@ -20,10 +19,10 @@ import fnmatch
 import re
 import cPickle as pickle
 import collections
+import shelve
+import time
 
-sourceDir = '/project2/marcotte/boulgakov/microscope/2014-July/2014-07-27/AS2_Atto647NSE_2aM_16hWash_647_OSS2_50Perc_trace002_flds001'
-imTIFF = 'AS2_Atto647NSE_2aM_16hWash_647_OSS2_50Perc_trace002_flds001t0001.tif'
-
+####################### BEGINNING OF CLASS #####################################
 
 class StarterFrame(object):
     """
@@ -63,6 +62,7 @@ class StarterFrame(object):
         ofile.close()
         return ofname
 
+
   
 class TiffStack(object):
     """
@@ -72,27 +72,95 @@ class TiffStack(object):
     frame can be obtained. 
     @ arg: fname - This is the filename for the first frame Tiff file
     @ arg: allPeakInfo - This is the list of information about all the peaks
+    Creates a shelve file
     """
     
-    def __init__(self,fname,allPeakCoords):
+    def __init__(self,fname,allPeakCoords,nbrFrames=600):
         self.fname = fname #Name of 1st frame file
         self.allPeakCoords = allPeakCoords
+        self.nbrFrames = nbrFrames #Total number of frames to consider
         self.traceDir = os.path.dirname(self.fname)
         self.allTiffs = [os.path.join(self.traceDir,f) for f in
                          os.listdir(self.traceDir) if f.endswith('.tif')]
-        self.allPeaks_dict = collections.defaultdict(list)
+        self.allTiffs.sort() #sort all the TIFF files
+        # Default dict
+        self.peak_frameInfo_dict = collections.defaultdict(list)
+        self.frame_peakInfo_dict = collections.defaultdict(list)
 
-    def isItPeak(self,avg_intensity,background_intensity,stdev_intensity):
-        snr = (avg_intensity - background_intensity)/stdev_intensity
-        if snr > 2: return 1
-        else: return 0
+    def makeShelveFiles(self):
+        shelveDir = os.path.join(self.traceDir,'shelve')
+
+        if not os.path.exists(shelveDir): os.makedirs(shelveDir)
+        makeShelveFile = lambda f: shelve.open(os.path.join(shelveDir,f))
+        frame_peakInfo_shelve, peak_frameInfo_shelve = map(makeShelveFile,
+                                                       ['frame_peakInfo_dict.shelve',
+                                                        'peak_frameInfo_dict.shelve'])
+
+        for k, v in self.peak_frameInfo_dict.items(): peak_frameInfo_shelve[k] = v
+        for k, v in self.frame_peakInfo_dict.items(): frame_peakInfo_shelve[k] = v
+        return shelveDir, frame_peakInfo_shelve, peak_frameInfo_shelve
+
+    def runThruFrames(self):
+        """ The function that routes the call of all functions to generate the
+        relevant two dictionaries.
+        """
+        frameNbr = 0
+        for tiffFile in self.allTiffs[0:self.nbrFrames]:
+        #for tiffFile in self.allTiffs[0:10]:
+            # tiffFile = Full Path of the tiff File
+            print "Processing : %s"%(tiffFile)
+            frameNbr += 1
+            allPeakInfo_frame = self.allPeakTraceIntensity(tiffFile,frameNbr)
+            self.frame_peakInfo_dict[tiffFile] = self.frameInfo(allPeakInfo_frame)
+
+    def frameInfo(self,allPeakInfo):
+        """ This function runs through the information of all peaks in that
+        particular frame and obtains statistics about the Frame like number of
+        active peaks, inactive peaks, avg peak intensity etc..
+        """
+            #allPeakInfo == [(x,y), FrameNbr, Peak Status (1,0), Full File path, Fname
+            # avgPeakIntensity (avg of 3x3 pxl), peakIntensity stdev, 
+            # avgBgIntensity (16 pxl around 3x3), stdev of the Background
+            # snr],[..]...
+        frameNbr, fname = allPeakInfo[0][1], allPeakInfo[0][4]
+        activePeaks = list()
+        inactivePeaks = list()
+
+        def _meanCalc(peakInt_list):
+            meanVal = lambda x: np.mean([item[x] for item in peakInt_list])
+            return meanVal(0),meanVal(1),meanVal(2)
+
+        for peak in allPeakInfo:
+            peakStatus = peak[2]
+            if peakStatus:
+                activePeaks.append([peak[5],peak[7],peak[9]])
+            else:
+                inactivePeaks.append([peak[5],peak[7],peak[9]])
+
+        nbrActivePeaks = len(activePeaks)
+        nbrInactivePeaks = len(inactivePeaks)
+            # frameInfo = [frameNbr, number of Active Peaks, number of Inactive Peaks,
+            # (Active : mean Peak Intensity, mean Background Intensity, mean snr),
+            # (Inactive : ....)
+        return [frameNbr, nbrActivePeaks,nbrInactivePeaks,_meanCalc(activePeaks),_meanCalc(inactivePeaks)]
 
     def allPeakTraceIntensity(self,f,frameNbr):
+        """ This function runs through all the peaks and then obtains the
+        details for each peak in the assigned frame...
+        """
+        allPeakInfo_frame = list()
         img = cv2.imread(f,-1)
+        
+        def _isItPeak(avg_intensity,background_intensity,stdev_intensity):
+            snr = (avg_intensity - background_intensity)/stdev_intensity
+            if snr > 2: return ( 1, snr)
+            else: return ( 0, snr)
+
         def _getPeakIntensity(peakAreaXY):
             peaklist = []
-            for (i,j) in peakAreaXY: peaklist.append(img[i,j])
-            return sum(peaklist)/len(peakAreaXY)            
+            for (i,j) in peakAreaXY: 
+                peaklist.append(img[i,j])
+            return np.mean(peaklist), np.std(peaklist)
         
         def _getBackgroundIntensity(bgAreaXY):
             bglist =  []
@@ -100,28 +168,35 @@ class TiffStack(object):
                 bglist.append(img[i,j])
             arr = np.array(bglist)
             return arr.mean(), arr.std()
-        
+      
 
         for (h,w) in self.allPeakCoords:
-
             sum_intensity = 0
             x,y = int(np.around(h)), int(np.around(w))
+            peakID = 'ID_'+str(x)+'_'+str(y)
             if not (x in range(2,509)  and y in range(2,509)): continue; # Takes care of borders
 
             all_peakAreaXY = [(i,j) for i in xrange(x-2,x+3) for j in xrange(y-2,y+3)]
             peakAreaXY = [(i,j) for i in xrange(x-1,x+2) for j in xrange(y-1,y+2)]
             bgAreaXY = [(i,j) for (i,j) in all_peakAreaXY if not (i,j) in peakAreaXY]
-            avg_peak_intensity = _getPeakIntensity(peakAreaXY)
-            avg_background_intensity,background_stdev = _getBackgroundIntensity(bgAreaXY)
+            avg_peak_intensity,peak_stdev = _getPeakIntensity(peakAreaXY)
+            avg_bg_intensity,bg_stdev = _getBackgroundIntensity(bgAreaXY)
             
-            peakStatus = self.isItPeak(avg_peak_intensity,avg_background_intensity,background_stdev)
-            #1 if snr>2 else 0
-            
-            #Peak ID - (x,y); Full File path, Fname, FrameNbr, peakStatus (1,0),
-            # avgPeakIntensity (avg of 3x3 pxl), avgBgIntensity (16 pxl around 3x3), stdev of the
-            # Background
-            self.allPeaks_dict[(x,y)].append([f,f.split('/')[-1],frameNbr,peakStatus, avg_peak_intensity,avg_background_intensity,background_stdev])
-        return True 
+            (peakStatus,snr) = _isItPeak(avg_peak_intensity,avg_bg_intensity,bg_stdev)
+            #Peak ID - ID_x_y: [(x,y), FrameNbr, Peak Status (1,0), Full File path, Fname
+            # avgPeakIntensity (avg of 3x3 pxl), peakIntensity stdev, 
+            # avgBgIntensity (16 pxl around 3x3), stdev of the Background
+            # snr
+            peakInfo = (
+                        [(x,y),frameNbr, peakStatus, f,f.split('/')[-1], 
+                         avg_peak_intensity,peak_stdev,
+                         avg_bg_intensity, bg_stdev, snr])
+            allPeakInfo_frame.append(peakInfo)
+            self.peak_frameInfo_dict[peakID].append(peakInfo)
+
+        return allPeakInfo_frame
+
+################## END OF CLASS #########################################                                      
 
 def locate(pattern, root=os.curdir):
     '''Locate all files matching supplied filename pattern in and
@@ -132,10 +207,99 @@ def locate(pattern, root=os.curdir):
             allFiles.append(os.path.join(path,filename))
     return allFiles
 
-def pickleIt(fname,obj):
-    ofile = open(fname,'w')
-    pickle.dump(obj,ofile)
-    ofile.close()
+def findInitialPeaks(dateStamp):
+    """ FINDING INITIAL PEAKS FUNCTION """
+
+    def _getPeakCoords(fname):
+        allPeakInfo = list()
+        frame = StarterFrame(fname)
+        results = frame.find_starterPeaks()
+        allPeakCoords = [frame.getPeakXY(peak) for peak in results]
+        pklFname = frame.picklePeakCoords(allPeakCoords)
+        return allPeakCoords, pklFname
+
+    sourceDir = '/project2/marcotte/boulgakov/microscope/2014-July'
+    pathDir = os.path.join(sourceDir,dateStamp)
+    allStarterTiffs = locate('*01.tif',pathDir)
+    firstFrameImages = [f for f in allStarterTiffs if re.search(r't[0]*01.tif',f)] #all time traces have a t###01.tif
+    for fname in firstFrameImages:
+        print "Processing Trace File : %s ...."%(fname)
+        allPeakCoords,pklFname = _getPeakCoords(fname)
+
+def runThroughFrames(fname):
+    """ USES THE PKL FILE OF THE FIRST STARTER FRAME. THEN RUNS THROUGH THE REST
+    OF THE TIFF FILE TO OBTAIN ALL THE PEAK AND FRAME INFORMATION """
+    allPeakCoords = pickle.load(open(fname))
+    traceFiles = TiffStack(fname,allPeakCoords)
+    traceFiles.runThruFrames()
+    shelveDir, frame_peakInfo_shelve, peak_frameInfo_shelve =  traceFiles.makeShelveFiles()
+    print "Number of Frames : %d"%(len(frame_peakInfo_shelve))
+    print "Number of Peaks Identified and Processed : %d"%(len(peak_frameInfo_shelve))
+
+def makeBashScript(dateStamp):
+    """ MAKES A TXT FILE THAT CONTAINS COMMAND LINE TO RUN THIS PYTHON PROGRAMME
+    WITH THE 'ANALYSE' PARAMETER. IT PASSES THE IDENTIFIIED PKL FILE
+    """
+    def _chuck(lst,n):
+        return [lst[i::n] for i in xrange(n)]
+    pattern = "*.tif.pkl"
+    sourceDir = '/project2/marcotte/boulgakov/microscope/2014-July'
+    pathDir = os.path.join(sourceDir,dateStamp)
+    allPklFiles = locate(pattern,pathDir)
+    bashDir = os.path.join(pathDir,'bashDir')
+    if not os.path.exists(bashDir): os.makedirs(bashDir)
+    chuckedList = _chuck(allPklFiles,4)
+    for i,item in enumerate(chuckedList):
+        ofname = os.path.join(bashDir,'ANALYSE_PYTHON_SCRIPTS.'+str(i)+'.sh')
+        print ofname
+        ofile = open(ofname,'w')
+        for pklFile in item:
+            cmd = ("python " 
+                    "/project/marcotte/jagannath/projectfiles/proteomics_single_molecule/imageAnalysis/pbKinetics.py "
+                    "ANALYSE "+
+                    pklFile + "\n"
+                  )
+            ofile.write(cmd)   
+
+
+
+
+if __name__ == '__main__':
+    [ARG, FILE] = sys.argv[1:]
+    t0 = time.clock()
+    if ARG == 'STARTER':
+        dateStamp = FILE 
+        findInitialPeaks(dateStamp)
+    elif ARG == 'ANALYSE': 
+        fname = FILE
+        print "In Analyse: PKL file"
+        runThroughFrames(fname)
+    elif ARG == 'GENERATE':
+        dateStamp = FILE
+        print "Generating a bash script to Analyse the pkl files"
+        makeBashScript(dateStamp)
+    else: SystemExit("Invalid Argument") 
+
+    t1 = time.clock()
+    print ("Script - %s \t Completed in %s secs \t %s"%(sys.argv, t1-t0, time.strftime("%d %b %Y %H:%M:%S",time.localtime()))
+          )
+
+
+"""
+##############################################
+TEST CASES
+
+def test_shelve(fname):
+    db = shelve.open(fname)
+    for k in db.keys()[0:10]:
+        print k, len(db[k])
+    db.close()
+
+def runThroughFrames_test(fname):
+    allPeakCoords = pickle.load(open(fname))
+    traceFiles = TiffStack(fname,allPeakCoords)
+    traceFiles.runThruFrames()
+    shelveDir, f1, f2 =  traceFiles.makeShelveFiles()
 
 
 def getPeakCoords(fname):
@@ -146,111 +310,8 @@ def getPeakCoords(fname):
     pklFname = frame.picklePeakCoords(allPeakCoords)
     return allPeakCoords, pklFname
 
-def getPeakTraceInfo(allPeakCoords,fname):
-    traceFiles = TiffStack(fname,allPeakCoords)
-    allTiffs = traceFiles.allTiffs
-    allTiffs.sort()
-    frameNbr = 0
-    for frameTiff in allTiffs:
-        frameNbr+=1
-        traceFiles.allPeakTraceIntensity(frameTiff,frameNbr)
-    ofname = fname+'.allPeaks_trace.dict.pkl'
-    ofile = open(ofname,'w')
-    pickle.dump(traceFiles.allPeaks_dict,ofile)
-    ofile.close()
-    return traceFiles.allPeaks_dict
 
-def getPeakInttraces(pklFname):
-    pklFname= (
-        '/project2/marcotte/boulgakov/microscope/2014-July/2014-07-27/AS2_Atto647NSE_2aM_16hWash_TFA1_647_Wat_50Perc_trace002_flds002/AS2_Atto647NSE_2aM_16hWash_TFA1_647_Wat_50Perc_trace002_flds002t0001.tif.allPeaks_trace.dict.pkl')    
-    peakInfo = pickle.load(open(pklFname))
-    """
-#Peak ID - (x,y); Full File path, Fname, FrameNbr, peakStatus (1,0),
-# avgPeakIntensity (avg of 3x3 pxl), avgBgIntensity (16 pxl around 3x3), stdev
-# of the background                                                          
-    """
-    ofname = pklFname+'.peakIntensityTraces.csv'
-    ofile = open(ofname,'w')
-    for k,v in peakInfo.items():
-        frameNbr, peakStatus,peakInt,bgInt = v[2],v[3],v[4],v[5]
-
-
-
-
-
-def analysePeaks():
-    import shelve
-
-    pklFname= (
-        '/project2/marcotte/boulgakov/microscope/2014-July/2014-07-27/AS2_Atto647NSE_2aM_16hWash_TFA1_647_Wat_50Perc_trace002_flds002/AS2_Atto647NSE_2aM_16hWash_TFA1_647_Wat_50Perc_trace002_flds002t0001.tif.allPeaks_trace.dict.pkl')    
-    peakInfo = pickle.load(open(pklFname))
-    """
-#Peak ID - (x,y); Full File path, Fname, FrameNbr, peakStatus (1,0),
-# avgPeakIntensity (avg of 3x3 pxl), avgBgIntensity (16 pxl around 3x3), stdev
-# of the background
-    """
-    traceDict = collections.defaultdict()
-    # PBleaching rates
-    totalPeaks = list() #Total active peaks/frame acrorss stack
-    avgPeakIntensity = list() # Avg peak Intensity/frame across stack
-    avgBGIntensity = list() # Avg background intensity/frame across stack
-    for k, val in peakInfo.items():
-        for v in val:
-            fname,peakStatus,peakInt,bgInt = v[1],v[3],v[4],v[5]
-            print fname, peakStatus, peakInt, bgInt
-
-    sys.exit(1)
-       
-
-
-        
-
-
-
-
-
-def main():
-    sourceDir = '/project2/marcotte/boulgakov/microscope/2014-July/2014-07-27'
-    allStarterTiffs = locate('*01.tif',sourceDir)
-    firstFrameImages = [f for f in allStarterTiffs if
-                        re.search(r't[0]*01.tif',f)] #all time traces have a t###01.tif
-    
-    for fname in firstFrameImages:
-        print fname
-        allPeakCoords,pklFname = getPeakCoords(fname)
-        # After the Peak Coords are generated 
-        allPeaks_dict = getPeakTraceInfo(allPeakCoords,fname)
-
-
-
-
-
-def test2():
-    fname = '/project2/marcotte/boulgakov/microscope/2014-July/2014-07-27/AS2_Atto647NSE_2aM_16hWash_647_OSS2_50Perc_trace002_flds002/AS2_Atto647NSE_2aM_16hWash_647_OSS2_50Perc_trace002_flds002t0001.tif.pkl'
-    allPeakCoords = pickle.load(open(fname))
-    traceFiles = TiffStack(fname,allPeakCoords)
-    allTiffs = traceFiles.allTiffs
-    allTiffs.sort()
-    frameNbr = 0
-    for frameTiff in allTiffs:
-        frameNbr+=1
-        traceFiles.allPeakIntensity(frameTiff,frameNbr)
-    ofname = fname+'.dict.pkl'
-    
-def test3():
-    fname = (
-    '/project2/marcotte/boulgakov/microscope/2014-July/2014-07-27/AS2_Atto647NSE_2aM_16hWash_647_OSS2_50Perc_trace002_flds003/AS2_Atto647NSE_2aM_16hWash_647_OSS2_50Perc_trace002_flds003t0001.tif.pkl'
-    )
-
-    print fname
-    
-    allPeakCoords = pickle.load(open(fname))
-    allPeaks_dict = getPeakTraceInfo(allPeakCoords,fname) 
-
-
-
-
-def test():
+def firstFrame_test():
     inputTIFF = sourceDir + '/' + imTIFF
     outputPNG = inputTIFF + '.png'
     cmd = "convert " + inputTIFF + " " + outputPNG
@@ -266,10 +327,6 @@ def test():
     results = pflibMP.fit_peaks([img],correlation_matrix = correlation_matrix)
     print results[0][1][1]
 
-#allPeaks_dict = collections.defaultdict(list)
-#test()
-#main()
-analysePeaks()
-#test3()
 
-#itest2()
+##############################################
+"""
